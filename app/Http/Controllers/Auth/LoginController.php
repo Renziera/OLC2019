e2a1b5bff27347e38e014df05b1fd9e32109f465
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-01-20 15:25:41 +0700

    Hmm

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    //biar login pake username
+    public function username()
+    {
+    return 'username';
+    }
+
 }
