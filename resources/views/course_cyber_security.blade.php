0c62610fb3e38f379243f85ffa90381d1eef82aa
rayhannr <44750699+rayhannr@users.noreply.github.com>
2019-03-23 16:26:30 +0700

    update faq

--- resources/views/course_cyber_security.blade.php
+++ resources/views/course_cyber_security.blade.php
@@ -51,6 +51,7 @@
             <a href="{{ route('jadwal') }}">Jadwal</a>
              
              <a href="/#course">Kelas</a>
+             <a href="{{ route('faq') }}">FAQ</a>
             <a href="{{ route('pembayaran') }}">Cek Pembayaran</a>
             @auth
             <a href="{{ route('admin') }}">Admin Panel</a>
@@ -80,6 +81,7 @@
 
                 <a href="{{ route('jadwal') }}">Jadwal</a>
                 <a href="/#course">Kelas</a>
+                <a href="{{ route('faq') }}">FAQ</a>
                 <a href="{{ route('pembayaran') }}">Cek Pembayaran</a>
                 @auth
                 <a href="{{ route('admin') }}">Admin Panel</a>
